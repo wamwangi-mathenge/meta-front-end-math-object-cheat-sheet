927dcf365aed239548c20b8d8810951421753b12
Brian <wamwangimathenge@gmail.com>
2023-07-26 11:53:11 +0300

    Round up any decimal number to the nearest integer
    This commit uses the Math.ceil() method to round up any decimal number
    to the nearest integer.

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ console.log(decimal);
 
 // Log the value of decimal MULTIPLIED BY !0
 console.log(decimal * 10)
+
+
+// Math ceil()
+
+// Round up any decimal number to the nearest integer
+var rounded1 = Math.ceil(0.0001);
+console.log(rounded1);
+var rounded2 = Math.ceil(0.5);
+console.log(rounded2);
+var rounded3 = Math.ceil(0.99);
+console.log(rounded3);
+var rounded4 = Math.ceil(1.01);
+console.log(rounded4);
+var rounded5 = Math.ceil(1.5);
+console.log(rounded5);
+var rounded6 = Math.ceil(2.99);
+console.log(rounded6);
