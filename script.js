4012faffaa937516f660a6ab9096a8a722a63e91
Brian <wamwangimathenge@gmail.com>
2023-07-26 11:58:58 +0300

    Generate a random integer
    This commit shows how to write a program that generates a random
    integer by generating a random decimal using the Math.random() method
    and rounding up the decimal using the Math.ceil() method.

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ console.log(decimal);
 // Log the value of decimal MULTIPLIED BY !0
 console.log(decimal * 10)
 
+console.log("--------------------------------------------------")
 
 // Math ceil()
 
@@ -28,3 +29,20 @@ var rounded5 = Math.ceil(1.5);
 console.log(rounded5);
 var rounded6 = Math.ceil(2.99);
 console.log(rounded6);
+
+
+console.log("---------------------------------------------")
+
+// Random integer
+
+// Get a random decimal number between 0 and 0.99
+// multiply it by 10 
+// and save it to a variable
+var randomDecimal = Math.random() * 10;
+console.log(randomDecimal)
+
+// Rounding up the value 
+var roundedRandom = Math.ceil(randomDecimal)
+
+// Log the value of the rounded to the decimal
+console.log(roundedRandom)
